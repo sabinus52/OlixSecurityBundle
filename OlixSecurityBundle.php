d792f033b2c66d63683515023eabfee0a0866efd
Olivier <sabinus52@gmail.com>
2015-01-26 16:19:51 +0100

    Surcharge du layout.html.twig

--- OlixSecurityBundle.php
+++ OlixSecurityBundle.php
@@ -6,4 +6,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OlixSecurityBundle extends Bundle
 {
+
+    /**
+     * @see \Symfony\Component\HttpKernel\Bundle\Bundle::getParent()
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+
 }
